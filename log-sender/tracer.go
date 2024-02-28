49e5ca42e28264e6e5090a9e6c14b89cbc24fe16
Artem Parygin <artem.parygin@gmail.com>
2024-02-28 21:52:29 +0100

    Fix import

--- log-sender/tracer.go
+++ log-sender/tracer.go
@@ -19,7 +19,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.16.0"
 	"go.opentelemetry.io/otel/trace"
 
-	pb "log-api/logs"
+	pb "github.com/artemparygin/troubleshooting/protos/logs"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
